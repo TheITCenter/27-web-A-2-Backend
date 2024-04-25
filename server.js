63985ea8c70453fcaf6baa9d5a376029ec2c7a10
Alex Mdz <ichialex5620mail.com>
2024-04-25 11:20:52 -0600

    Cambios del Server y nombre de un Documento

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import express from "express";
+import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import plataformRoutes from "./routes/plataformRoutes.js";
+import genderRouter from "./routes/genderRoutes.js";
 
-const api = express(8000, () => {
+const api = express()
+api.use(express.json())
+
+api.listen(8000, () => {
     console.log("Server is running on port 8000");
 })
+
+api.use('/auths', authRouter)
+api.use('/users', userRouter)
+api.use('/plataforms', plataformRoutes)
+api.use('/gender', genderRouter)
